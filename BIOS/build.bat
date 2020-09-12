3cb77c79163348b50758b725a784d0e12af251c2
SP <svpetry@gmail.com>
2020-09-12 19:06:21 +0200

    Fixed build batch file

--- BIOS/build.bat
+++ BIOS/build.bat
@@ -1,15 +1,9 @@
-@rem build.bat
+nasm -f bin src\resetvector.asm -o resetvector.bin
 
-del *.obj
-del *.bin
-del *.err
+copy src\*.h dosbox\local /b /v /y
+copy src\*.c dosbox\local /b /v /y
+DOSBox.exe dosbox\setup.bat
+copy dosbox\local\bios0.bin . /b /v /y
 
-cls
-
-nasm -f bin resetvector.asm -o resetvector.bin
-nasm -f bin initializer.asm -o initializer.bin
-
-call buildbios.bat
-
-del *.obj
 BiosBuilder
+pause

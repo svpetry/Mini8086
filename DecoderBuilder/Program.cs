85ba501075bf648e24f33343113232df79fe8e4c
SP <svpetry@gmail.com>
2020-10-07 06:06:23 +0200

    Updated decoderbuilder. Added character ROM generation.

--- DecoderBuilder/Program.cs
+++ DecoderBuilder/Program.cs
@@ -1,22 +1,41 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System.IO;
 
 namespace DecoderBuilder
 {
+    enum Signal
+    {
+        MEMRD = 1,
+        MEMWR = 2,
+        ROMRD = 4,
+        IO0 = 8,
+        IO1 = 16,
+        IO_DBG = 32,
+        VGA_MEM = 64,
+        VGA_IO = 128
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            GenerateDecoder(@"..\..\..\ROMs\decoder.bin");
+            GenerateCharacterRom(@"..\..\..\ROMs\charrom.bin");
+        }
+
+        private static void GenerateDecoder(string filePath)
+        {
             var data = new byte[65536];
 
             for (var addr = 0; addr < data.Length; addr++)
-                data[addr] = GenOutput(addr);
+                data[addr] = GenDecoderOutput(addr);
 
-            File.WriteAllBytes("decoder.bin", data);
+            File.WriteAllBytes(filePath, data);
         }
 
-        private static byte GenOutput(int addr)
+        private static byte GenDecoderOutput(int addr)
         {
-            var data = 0xFF;
+            byte data = 0xFF;
 
             var M_IO = (addr & (1 << 13)) > 0 ? 1 : 0;
             var _RD = (addr & (1 << 14)) > 0 ? 1 : 0;
@@ -28,20 +47,24 @@ namespace DecoderBuilder
                 var memAddr = (addr & 0b0001111000000000) << 7;
 
                 // RAM
-                if (memAddr >= 0 && memAddr <= 0x3FFFF)
+                if (memAddr >= 0 && memAddr <= 0x7FFFF)
                 {
                     if (_RD == 0)
-                        data &= 0b11111110;
+                        SetSignal(ref data, Signal.MEMRD);
                     if (_WR == 0)
-                        data &= 0b11111101;
+                        SetSignal(ref data, Signal.MEMWR);
                 }
 
                 // ROM
                 if (memAddr >= 0xE0000 && memAddr <= 0xFFFFF)
                 {
                     if (_RD == 0)
-                        data &= 0b11111011;
+                        SetSignal(ref data, Signal.ROMRD);
                 }
+
+                // VGA memory
+                if (memAddr >= 0xC0000 && memAddr <= 0xDFFFF)
+                    SetSignal(ref data, Signal.VGA_MEM);
             }
             else
             {
@@ -50,31 +73,59 @@ namespace DecoderBuilder
                 if (ioAddr >= 0x000 && ioAddr <= 0x007)
                 {
                     // IO0
-                    data &= 0b11110111;
+                    SetSignal(ref data, Signal.IO0);
                 }
                 if (ioAddr >= 0x008 && ioAddr <= 0x00F)
                 {
                     // IO1
-                    data &= 0b11101111;
+                    SetSignal(ref data, Signal.IO1);
                 }
                 if (ioAddr >= 0x010 && ioAddr <= 0x017)
                 {
-                    // IO2
-                    data &= 0b11011111;
+                    // IO_DBG
+                    SetSignal(ref data, Signal.IO_DBG);
                 }
-                if (ioAddr >= 0x018 && ioAddr <= 0x01F)
+                if (ioAddr >= 0x028 && ioAddr <= 0x02F)
                 {
-                    // IO3
-                    data &= 0b10111111;
-                }
-                if (ioAddr >= 0x020 && ioAddr <= 0x027)
-                {
-                    // IO4
-                    data &= 0b01111111;
+                    // VGA_IO
+                    SetSignal(ref data, Signal.VGA_IO);
                 }
             }
 
             return (byte)data;
+        }
+
+        private static void SetSignal(ref byte data, Signal signal)
+        {
+            data &= (byte)~signal;
+        }
+
+        private static void GenerateCharacterRom(string filePath)
+        {
+            var data = new byte[65536];
+            var dataIdx = 0;
+            const int invOffset = 128 * 16;
+
+            var bmp = new Bitmap(@"..\..\..\VGA\Characters.png");
+            for (var charNo = 0; charNo < 128; charNo++)
+            {
+                var charX = (charNo % 32) * 16;
+                var charY = (charNo / 32) * 16;
+                for (var y = 0; y < 16; y++)
+                {
+                    byte d = 0;
+                    for (var x = 0; x < 8; x++)
+                    {
+                        if (bmp.GetPixel(charX + x, charY + y).G == 0)
+                            d = (byte)(d | (1 << x));
+                    }
+                    data[dataIdx] = d;
+                    data[invOffset + dataIdx] = (byte)~d;
+                    dataIdx++;
+                }
+            }
+
+            File.WriteAllBytes(filePath, data);
         }
     }
 }

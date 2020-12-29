132ab7a6d1e503f233a4734195bea2faad291795
Sven Petry <svpetry@gmail.com>
2020-12-29 06:33:39 +0100

    Added I/O decoder

--- DecoderBuilder/Program.cs
+++ DecoderBuilder/Program.cs
@@ -16,11 +16,21 @@ namespace DecoderBuilder
         VGA_IO = 128
     }
 
+    enum IoSignal
+    {
+        CS_UART = 1,
+        CS_8042 = 2,
+        CS_DRIVE = 4,
+        WR_BUFFER = 8,
+        CS_I2C = 16
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             GenerateDecoder(@"..\..\..\ROMs\decoder.bin");
+            GenerateIoDecoder(@"..\..\..\ROMs\iodecoder.bin");
             GenerateCharacterRom(@"..\..\..\ROMs\charrom.bin");
         }
 
@@ -30,6 +40,16 @@ namespace DecoderBuilder
 
             for (var addr = 0; addr < data.Length; addr++)
                 data[addr] = GenDecoderOutput(addr);
+
+            File.WriteAllBytes(filePath, data);
+        }
+
+        private static void GenerateIoDecoder(string filePath)
+        {
+            var data = new byte[65536];
+
+            for (var addr = 0; addr < data.Length; addr++)
+                data[addr] = GenIoDecoderOutput(addr);
 
             File.WriteAllBytes(filePath, data);
         }
@@ -54,21 +74,21 @@ namespace DecoderBuilder
                     if (memAddr >= 0 && memAddr <= 0x7FFFF)
                     {
                         if (_RD == 0)
-                            SetSignal(ref data, Signal.MEMRD);
+                            SetSignal(ref data, (byte)Signal.MEMRD);
                         if (_WR == 0)
-                            SetSignal(ref data, Signal.MEMWR);
+                            SetSignal(ref data, (byte)Signal.MEMWR);
                     }
 
                     // ROM
                     if (memAddr >= 0xE0000 && memAddr <= 0xFFFFF)
                     {
                         if (_RD == 0)
-                            SetSignal(ref data, Signal.ROMRD);
+                            SetSignal(ref data, (byte)Signal.ROMRD);
                     }
 
                     // VGA memory
                     if (memAddr >= 0xC0000 && memAddr <= 0xDFFFF)
-                        SetSignal(ref data, Signal.VGA_MEM);
+                        SetSignal(ref data, (byte)Signal.VGA_MEM);
                 }
                 else
                 {
@@ -77,22 +97,22 @@ namespace DecoderBuilder
                     if (ioAddr >= 0x040 && ioAddr <= 0x047)
                     {
                         // IO_TIMER
-                        SetSignal(ref data, Signal.IO_TIMER);
+                        SetSignal(ref data, (byte)Signal.IO_TIMER);
                     }
                     if (ioAddr >= 0x020 && ioAddr <= 0x027)
                     {
                         // IO_PIC
-                        SetSignal(ref data, Signal.IO_PIC);
+                        SetSignal(ref data, (byte)Signal.IO_PIC);
                     }
                     if (ioAddr >= 0x010 && ioAddr <= 0x017)
                     {
                         // IO_DBG
-                        SetSignal(ref data, Signal.IO_DBG);
+                        SetSignal(ref data, (byte)Signal.IO_DBG);
                     }
                     if (ioAddr >= 0x050 && ioAddr <= 0x057)
                     {
                         // VGA_IO
-                        SetSignal(ref data, Signal.VGA_IO);
+                        SetSignal(ref data, (byte)Signal.VGA_IO);
                     }
                 }
             }
@@ -100,7 +120,54 @@ namespace DecoderBuilder
             return (byte)data;
         }
 
-        private static void SetSignal(ref byte data, Signal signal)
+        private static byte GenIoDecoderOutput(int addr)
+        {
+            byte data = 0xFF;
+
+            var M_IO = (addr & (1 << 13)) > 0 ? 1 : 0;
+            var _RD = (addr & (1 << 14)) > 0 ? 1 : 0;
+            var _WR = (addr & (1 << 15)) > 0 ? 1 : 0;
+
+            if (_RD == 0 || _WR == 0)
+            {
+
+                if (M_IO == 0)
+                {
+                    // I/O access
+                    var ioAddr = (addr & 0xFF) << 3;
+
+                    if (ioAddr >= 0x060 && ioAddr <= 0x067)
+                    {
+                        // CS_8042
+                        SetSignal(ref data, (byte)IoSignal.CS_8042);
+                    }
+                    if (ioAddr >= 0x070 && ioAddr <= 0x077)
+                    {
+                        // CS_I2C
+                        SetSignal(ref data, (byte)IoSignal.CS_I2C);
+                    }
+                    if (ioAddr >= 0x080 && ioAddr <= 0x087)
+                    {
+                        // CS_UART
+                        SetSignal(ref data, (byte)IoSignal.CS_UART);
+                    }
+                    if (ioAddr >= 0x090 && ioAddr <= 0x097)
+                    {
+                        // CS_DRIVE
+                        SetSignal(ref data, (byte)IoSignal.CS_DRIVE);
+                    }
+                    if (ioAddr >= 0x0400 && ioAddr <= 0x7FF)
+                    {
+                        // WR_BUFFER
+                        SetSignal(ref data, (byte)IoSignal.WR_BUFFER);
+                    }
+                }
+            }
+
+            return (byte)data;
+        }
+
+        private static void SetSignal(ref byte data, byte signal)
         {
             data &= (byte)~signal;
         }

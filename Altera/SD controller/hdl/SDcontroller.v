e56eb5d61d22ea93006c476763b435f07850707e
Sven Petry <svpetry@gmail.com>
2021-03-26 06:13:06 +0100

    Fixed SD controller.

--- Altera/SD controller/hdl/SDcontroller.v
+++ Altera/SD controller/hdl/SDcontroller.v
@@ -11,7 +11,7 @@ module SDcontroller(
 	output reg SD_IRQ,
 	output LD_GN,
 	output LD_RD,
-	output LD_BL,
+	output reg LD_BL,
 	output CS_UART,
 	output CS_8042,
 	output CS_I2C,
@@ -20,7 +20,7 @@ module SDcontroller(
 
 	output SD_CS,
 	input MISO,
-	output reg SCLK,
+	output SCLK,
 	output reg MOSI
 );
 
@@ -43,18 +43,21 @@ module SDcontroller(
 // internal
 reg [7:0] control = 8'b00001000;		// control register (0..2: clock divider, 3: SD_CS, 4: LED gn, 5: LED rd)
 wire [7:0] status; 						// status register (0: busy)
-reg [7:0] txdata;
-reg [7:0] rxdata;
-reg [7:0] data_out;
-reg [7:0] data_out_en;
-reg [7:0] shift_register;
-reg [3:0] count;
+reg [7:0] txdata = 0;
+reg [7:0] rxdata = 0;
+reg [7:0] data_out = 0;
+reg [7:0] data_out_en = 8'bz;
+reg [7:0] shift_register = 0;
+reg [3:0] count = 0;
 reg [7:0] clk_divide = 0;
-reg slave_cs = 1;
-reg spi_word_send;
-reg [1:0]write_active;
+reg spi_active = 0;
+reg spi_start = 0;
+reg [1:0] write_active = 0;
+reg [1:0] sclk_reg = 0;
+reg [11:0] led_count = 0;
 
 wire spi_clk_gen;
+reg sclk_int = 0;
 wire [2:0] divide_factor = control[2:0];
 
 wire [1:0] irq_ctrl;
@@ -63,20 +66,58 @@ wire cs_drive;
 assign SD_CS = control[3];
 assign LD_GN = control[4];
 assign LD_RD = control[5];
-assign status[0] = ~slave_cs;
-assign status[1] = CARD_DET;
+assign status[0] = spi_active;
+assign status[1] = ~CARD_DET;
 assign irq_ctrl = control[7:6];
 
 // SPI clock
 assign spi_clk_gen = clk_divide[divide_factor];
 
 // clock divider for SPI clock
-always @ (negedge clk)
+always @(posedge clk)
 begin
     clk_divide = clk_divide + 8'd1;
 end
 
+// generate the SPI clock
+always @(posedge spi_clk_gen)
+begin
+	sclk_int <= ~sclk_int;
+end
+assign SCLK = spi_active ? sclk_int : 0;
+
 Decoder decoder(AL[7:0], WR, RD, M_IO, CS_UART, CS_8042, CS_I2C, cs_drive);
+
+// blue I/O led
+always @(posedge clk)
+begin
+	if (CS_UART || CS_8042 || CS_I2C || cs_drive)
+		led_count = 12'hFFF;
+	
+	if (led_count > 0)
+	begin
+		led_count = led_count - 12'd1;
+		LD_BL <= 1;
+	end
+	else
+		LD_BL <= 0;
+end
+
+/*
+always @(posedge clk)
+begin
+	if (init_done)
+	begin
+		LD_BL = ~CARD_DET;
+	
+	end
+	else begin
+		LD_BL = 0;
+		if (clk_divide == 8'hFF)
+			init_done = 1;
+	end
+end
+*/
 
 // registers
 // 0: control (read, write)
@@ -84,13 +125,19 @@ Decoder decoder(AL[7:0], WR, RD, M_IO, CS_UART, CS_8042, CS_I2C, cs_drive);
 // 2: DATA (read, write)
 always @(posedge clk)
 begin
+	write_active[1] = write_active[0];
+	write_active[0] = 0;
+
 	if (cs_drive)
 	begin
 		if (~WR)
 		begin
 			case (ADDR)
 				2'b00: control <= DATA;
-				2'b10: txdata <= DATA;
+				2'b10: begin
+					txdata = DATA;
+					write_active[0] = 1;
+				end
 			endcase
 		end
 		else if (~RD)
@@ -104,25 +151,53 @@ begin
 	end
 end
 
-/* Reading the MISO line and shifting */
-always @(posedge SCLK or posedge spi_word_send)
+always @(negedge clk)
 begin
-	if (spi_word_send)
-		shift_register[7:0] = txdata;
-	else begin
-		shift_register = shift_register << 1;
-		shift_register[0] <= MISO;
-   end
-end
+	sclk_reg[1] = sclk_reg[0];
+	sclk_reg[0] = sclk_int;
+	
+	if (~write_active[0] && write_active[1])
+		spi_start = 1;
 
-/* Writing the MOSI */
-always @(negedge SCLK or posedge spi_word_send)
-begin
-	if (spi_word_send)
-		MOSI = txdata[7];
-	else
-		MOSI = shift_register[7];
+	// negedge sclk_int
+	if (~sclk_reg[0] && sclk_reg[1])
+	begin
+		MOSI = 0;
+		
+		if (spi_start)
+		begin
+			shift_register = txdata;
+			spi_active = 1;
+			count = 0;
+			MOSI = txdata[7];
+		end
+		else if (spi_active)
+			MOSI = shift_register[7];
+
+		if (spi_active)
+		begin
+			if (count == 8)
+			begin
+				spi_active = 0;
+				rxdata = shift_register;
+			end
+			else
+				shift_register = shift_register << 1;
+		end
+	end
+	
+	// posedge sclk_int
+	if (sclk_reg[0] && ~sclk_reg[1])
+	begin
+		if (spi_active)
+		begin
+			spi_start = 0;
+			shift_register[0] = MISO;
+			count = count + 4'd1;
+		end
+	end
 end
+assign DEN = cs_drive;
 
 // interrupt control
 always @(posedge clk)
@@ -133,7 +208,7 @@ begin
 //		end
 //	endcase
 //
-//	if (spi_word_send)
+//	if (spi_start)
 //	begin
 //		if (irq_ctrl == 2)
 //			SD_IRQ = 0;
@@ -146,59 +221,14 @@ begin
 //	if (irq_ctrl == 3)
 end
 
-// read buffer
-always @(posedge slave_cs)
-begin
-	rxdata = shift_register; // updating read buffer
-end
-
-/* New SPI word starts when the transmit buffer is updated */
-always @(posedge clk)
-begin
-	if (spi_word_send)
-		slave_cs = 0;
-   else if (count == 8 & ~SCLK)
-		slave_cs = 1;
-end
-
-/* New Spi word is intiated after transmit buffer has been updated */
-always @(posedge clk)
-begin
-	write_active[1] = write_active[0];
-	if (cs_drive & ~WR & ADDR == 2'b10)
-		write_active[0] = 1;
-	else
-		write_active[0] = 0;
-	spi_word_send <= ~write_active[0] && write_active[1];
-end
-
-/* Generating the SPI clock */
-always @(posedge spi_clk_gen)
-begin
-	if (~slave_cs)
-		SCLK = ~SCLK;
-   else
-		SCLK = 0;
-end
-
-/* Counting SPI word length */
-always @(posedge SCLK or posedge slave_cs)
-begin
-	if (slave_cs)
-		count = 0;
-   else
-		count = count + 4'd1;
-end
-
-/* Controlling the DATA out enable */
-always @(RD or cs_drive or data_out)
+// data out
+always @(*)
 begin
 	if (~RD && cs_drive)
 		data_out_en <= data_out;
 	else
 		data_out_en <= 8'bz;
 end
-
 assign DATA = data_out_en;
 
 endmodule
--- Altera/SD controller/testbenches/SDcontroller_tb.v
+++ Altera/SD controller/testbenches/SDcontroller_tb.v
@@ -2,13 +2,14 @@
 module Sdcontroller_tb;
 
 // inputs
-reg cs_drive = 1, rd = 1, wr = 1, cs_buffer = 1, card_det, clk;
+reg m_io = 1, rd = 1, wr = 1, cs_buffer = 1, card_det, clk;
 reg [1:0] addr;
+reg [7:0] al = 8'b00010010; // SD controller I/O address
 reg miso;
 
 // outputs
-wire den, sd_busy, ram_oe, ram_we, sd_irq, ld_n, ld_rd;
-wire [8:0] ram_addr;
+wire den, sd_irq, ld_n, ld_rd, ld_bl;
+wire cs,uart, cs_8042, cs_i2c;
 wire sd_cs, sclk, mosi;
 
 // inout
@@ -20,23 +21,22 @@ reg [7:0] data_out;
 
 // Instantiate UUT
 SDcontroller UUT (
-    .CS_DRIVE(cs_drive),
+    .M_IO(m_io),
     .RD(rd),
     .WR(wr),
-    .CS_BUFFER(cs_buffer),
     .ADDR(addr),
+    .AL(al),
     .CARD_DET(card_det),
     .clk(clk),
     .DEN(den),
-    .SD_BUSY(sd_busy),
-    .RAM_OE(ram_oe),
-    .RAM_WE(ram_we),
     .SD_IRQ(sd_irq),
     .LD_GN(ld_gn),
     .LD_RD(ld_rd),
-    .RAM_ADDR(ram_addr),
+    .LD_BL(ld_bl),
+    .CS_UART(cs_uart),
+    .CS_8042(cs_8042),
+    .CS_I2C(cs_i2c),
     .DATA(data),
-    .RAM_DATA(ram_data),
     .SD_CS(sd_cs),
     .MISO(miso),
     .SCLK(sclk),
@@ -58,11 +58,11 @@ initial begin
     data_out = 8'hFF;
     data_out_active = 1;
     addr = 2'd2;
-    cs_drive = 0;
+    m_io = 0;
     wr = 0;
     #100
     data_out_active = 0;
-    cs_drive = 1;
+    m_io = 1;
     wr = 1;
 
     #2500
@@ -70,11 +70,11 @@ initial begin
     data_out = 8'h40;
     data_out_active = 1;
     addr = 2'd2;
-    cs_drive = 0;
+    m_io = 0;
     wr = 0;
     #100
     data_out_active = 0;
-    cs_drive = 1;
+    m_io = 1;
     wr = 1;
     
     #2500
@@ -83,4 +83,4 @@ initial begin
 end
 
 
-endmodule;
+endmodule

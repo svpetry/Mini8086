cb561087cc7e9ab01cc47cf87cac6c2b9949d07c
SP <svpetry@gmail.com>
2021-05-05 06:22:02 +0200

    Updated BIOS build script

--- Apps/BIOS/build.sh
+++ Apps/BIOS/build.sh
@@ -3,10 +3,10 @@ echo '#define BUILD "'$timestamp'"' > ./src/build.h
 
 ia16-elf-gcc -Os -c -o crt0.o ./src/CRT0.S
 ia16-elf-gcc -Os -mcmodel=small -fno-jump-tables -nostartfiles \
-    -mtune=i8086 -mno-segment-relocation-stuff -T ./src/BIOS.LD -o ./bin/b.bin \
+    -mtune=i8086 -mno-segment-relocation-stuff -mfar-function-if-far-return-type -T ./src/BIOS.LD -o ./bin/b.bin \
     ./src/bios.c ./src/io.c ./src/boot.c ./src/start.c \
-    ./src/ff.c ./src/diskio.c ./src/biosfunc.c ./src/sd.c ./src/keyboard.c ./src/keylayouts.c \
-    ./src/filebrowser.c ./src/biossetup.c \
+    ./src/ff.c ./src/diskio.c ./src/bios_api.c ./src/sd.c ./src/keyboard.c ./src/keylayouts.c \
+    ./src/filebrowser.c ./src/bios_setup.c ./src/bios_fsapi.c \
     ../Lib/lowlevel.c ../Lib/strutils.c ../Lib/utils.c ../Lib/i2c.c ../Lib/eeprom.c ../Lib/ds1307.c \
     ../Lib/screen.c ../Lib/lcd.c ../Lib/bmp280.c
 rm crt0.o

38f295a7c7ec1710ee4e0e68a7a1425202c46fe6
SP <svpetry@gmail.com>
2021-05-26 13:02:24 +0200

    Changed gcc optimization from Os to O2

--- Apps/BIOS/build.sh
+++ Apps/BIOS/build.sh
@@ -1,8 +1,8 @@
 timestamp=$(date "+%Y%m%d.%H%M%S")
 echo '#define BUILD "'$timestamp'"' > ./src/build.h
 
-ia16-elf-gcc -Os -c -o crt0.o ./src/CRT0.S
-ia16-elf-gcc -Os -mcmodel=small -fno-jump-tables -nostartfiles \
+ia16-elf-gcc -c -o crt0.o ./src/CRT0.S
+ia16-elf-gcc -O2 -mcmodel=small -fno-jump-tables -nostartfiles \
     -mtune=i8086 -mno-segment-relocation-stuff -mfar-function-if-far-return-type -T ./src/BIOS.LD -o ./bin/b.bin \
     ./src/bios.c ./src/io.c ./src/boot.c ./src/start.c \
     ./src/ff.c ./src/diskio.c ./src/bios_api.c ./src/sd.c ./src/keyboard.c ./src/keylayouts.c \

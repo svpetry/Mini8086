9c101bf8a31e5116b3fc44996829f22a676f61e9
SP <svpetry@gmail.com>
2023-07-14 22:35:38 +0200

    Fixed build.

--- Apps/BIOS/build.sh
+++ Apps/BIOS/build.sh
@@ -2,7 +2,7 @@ timestamp=$(date "+%Y%m%d.%H%M%S")
 echo '#define BUILD "'$timestamp'"' > ./src/build.h
 
 ia16-elf-gcc -c -o crt0.o ./src/CRT0.S
-ia16-elf-gcc -O2 -mcmodel=small -fno-jump-tables -nostartfiles \
+ia16-elf-gcc -O2 -mcmodel=small -fno-jump-tables -nostartfiles -mseparate-code-segment -Wl,-N -nostdlib \
     -mtune=i8086 -mno-segment-relocation-stuff -mfar-function-if-far-return-type -T ./src/BIOS.LD -o ./bin/b.bin \
     ./src/bios.c ./src/io.c ./src/boot.c ./src/start.c \
     ./src/ff.c ./src/diskio.c ./src/bios_api.c ./src/sd.c ./src/keyboard.c ./src/keylayouts.c \
